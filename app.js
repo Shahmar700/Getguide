d9d9afa8679ec5e44a8ee69c687fbf3ae75e6fd3
Shahmar <shahmar700@gmail.com>
2023-01-29 08:23:51 +0400

    update and add new pages

--- app.js
+++ app.js
@@ -13,19 +13,37 @@ const answer = document.querySelectorAll(".faq-answer");
   });
 });
 
-// const i = [...document.querySelectorAll(".fa-plus")];
-// i.map((i) => {
-//   i.addEventListener("click", (e) => {
-//     //  if (p.style.height != 0) {
+// Giudes Filters
 
-//     //  } else {
-//     //    i.style.transform = "rotate(180deg)";
-//     //  }
-//     [...document.querySelectorAll(".faq-answer")].map(
-//       (a) => (a.style.height = 0)
-//     );
-//     i.style.transform = "rotate(45deg)";
-//     let p = e.target.nextElementSibling;
-//     p.style.height = `${p.scrollHeight}px`;
-//   });
-// });
+const langFilterHead = document.querySelectorAll(".langFilterHead");
+const langChevron = document.querySelector("#langChevron");
+[...langFilterHead].map((h) => {
+  h.addEventListener("click", () => {
+    let dropdown = h.nextElementSibling;
+    dropdown.classList.toggle("dropdownHeight");
+    langChevron.classList.toggle("rotate180");
+  });
+});
+
+const activityFilterHead = document.querySelectorAll(".activityFilterHead");
+const activityChevron = document.querySelector("#activityChevron");
+[...activityFilterHead].map((h) => {
+  h.addEventListener("click", () => {
+    let dropdown = h.nextElementSibling;
+    dropdown.classList.toggle("dropdownHeight");
+    activityChevron.classList.toggle("rotate180");
+  });
+});
+
+const priceFilterHead = document.querySelectorAll(".priceFilterHead");
+const priceChevron = document.querySelector("#priceChevron");
+[...priceFilterHead].map((h) => {
+  h.addEventListener("click", () => {
+    let dropdown = h.nextElementSibling;
+    dropdown.classList.toggle("dropdownHeight");
+    priceChevron.classList.toggle("rotate180");
+  });
+});
+
+
+// Guide Filetrs End 
