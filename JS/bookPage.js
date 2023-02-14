6b4ddb722135d40b2fb9da580d9bfd57efc0034a
Shahmar <shahmar700@gmail.com>
2023-02-14 17:26:29 +0400

    Book Page Finish and SignUp&Login Update

--- JS/bookPage.js
+++ JS/bookPage.js
@@ -1,21 +1,6 @@
-let infoHeader = document.querySelectorAll(".infoHeader");
-
-[...infoHeader].map((i) => {
-  i.addEventListener("click", (e) => {
-    let info = e.target.nextElementSibling;
-    info.classList.toggle("dropdownHide");
-  });
-});
-
-let aboutInfo = document.querySelector(".aboutInfo");
-let whyChooseInfo = document.querySelector(".whyChooseInfo");
-let aboutChevron = document.getElementById("aboutChevron");
-let whyChevron = document.getElementById("langChevron");
-
-aboutInfo.addEventListener("click", () => {
-  aboutChevron.classList.toggle("rotate180");
-});
-
-whyChooseInfo.addEventListener("click", () => {
-  whyChevron.classList.toggle("rotate180");
+$(".infoHeader").click(function () {
+  $(".infoResponse").slideUp(400);
+  $(this).find(".infoResponse").slideDown(500);
+  $(".rotate180").removeClass("rotate180");
+  $(this).find("i").addClass("rotate180");
 });
