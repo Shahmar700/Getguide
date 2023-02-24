291f327ed04c71e226a764544984111b03e9618d
Shahmar <shahmar700@gmail.com>
2023-02-24 14:14:20 +0400

    sigUp&Login Btn Disable

--- JS/signup.js
+++ JS/signup.js
@@ -1,19 +1,24 @@
 let guide = document.getElementById("guide");
 let company = document.getElementById("company");
 let driver = document.getElementById("driver");
-
 guide.addEventListener("click", () => {
   guide.checked = true;
   company.checked = false;
   driver.checked = false;
+  document.getElementById("reqSpan").style.display = "none";
+  document.getElementById("signBtn").disabled = false;
 });
 company.addEventListener("click", () => {
   company.checked = true;
   guide.checked = false;
   driver.checked = false;
+  document.getElementById("reqSpan").style.display = "none";
+  document.getElementById("signBtn").disabled = false;
 });
 driver.addEventListener("click", () => {
   driver.checked = true;
   guide.checked = false;
   company.checked = false;
+  document.getElementById("reqSpan").style.display = "none";
+  document.getElementById("signBtn").disabled = false;
 });
