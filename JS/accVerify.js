83eaf2068e23bf20be776a50fa3700c34b172c72
Shahmar <shahmar700@gmail.com>
2023-03-02 13:15:44 +0400

    dashboard

--- JS/accVerify.js
+++ JS/accVerify.js
@@ -1,56 +1,56 @@
-let firstStep = document.querySelector(".firstStep");
-// Second Side
-let secondStep = document.querySelector(".secondStep");
-let secondPrev = document.querySelector(".secondStep .prevBtn");
-let secondNext = document.querySelector(".secondStep .nextBtn");
-// Third Side
-let thirdStep = document.querySelector(".thirdStep");
-let thirdPrev = document.querySelector(".thirdStep .prevBtn");
-let thirdNext = document.querySelector(".thirdStep .nextBtn");
-// Fourth Side
-let fourthStep = document.querySelector(".fourthStep");
-let startBtn = document.querySelector(".firstBtn");
-let successLine = document.querySelector(".successLine");
-let successAfter = window.getComputedStyle(successLine, "::after");
-let twentyFive = document.getElementById("twentyFive");
-let fifty = document.getElementById("fifty");
-let seventyFive = document.getElementById("seventyFive");
-startBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  firstStep.classList.add("none");
-  secondStep.classList.remove("none");
-  successLine.style.setProperty("--width", `35%`);
-  twentyFive.style.backgroundColor = "#3366FF";
-});
+// let firstStep = document.querySelector(".firstStep");
+// // Second Side
+// let secondStep = document.querySelector(".secondStep");
+// let secondPrev = document.querySelector(".secondStep .prevBtn");
+// let secondNext = document.querySelector(".secondStep .nextBtn");
+// // Third Side
+// let thirdStep = document.querySelector(".thirdStep");
+// let thirdPrev = document.querySelector(".thirdStep .prevBtn");
+// let thirdNext = document.querySelector(".thirdStep .nextBtn");
+// // Fourth Side
+// let fourthStep = document.querySelector(".fourthStep");
+// let startBtn = document.querySelector(".firstBtn");
+// let successLine = document.querySelector(".successLine");
+// let successAfter = window.getComputedStyle(successLine, "::after");
+// let twentyFive = document.getElementById("twentyFive");
+// let fifty = document.getElementById("fifty");
+// let seventyFive = document.getElementById("seventyFive");
+// startBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   firstStep.classList.add("none");
+//   secondStep.classList.remove("none");
+//   successLine.style.setProperty("--width", `35%`);
+//   twentyFive.style.backgroundColor = "#3366FF";
+// });
 
-secondPrev.addEventListener("click", (e) => {
-  e.preventDefault();
-  secondStep.classList.add("none");
-  firstStep.classList.remove("none");
-  successLine.style.setProperty("--width", `0%`);
-  twentyFive.style.backgroundColor = "transparent";
-});
+// secondPrev.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   secondStep.classList.add("none");
+//   firstStep.classList.remove("none");
+//   successLine.style.setProperty("--width", `0%`);
+//   twentyFive.style.backgroundColor = "transparent";
+// });
 
-secondNext.addEventListener("click", (e) => {
-  e.preventDefault();
-  secondStep.classList.add("none");
-  thirdStep.classList.remove("none");
-  successLine.style.setProperty("--width", `65%`);
-  fifty.style.backgroundColor = "#3366FF";
-});
+// secondNext.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   secondStep.classList.add("none");
+//   thirdStep.classList.remove("none");
+//   successLine.style.setProperty("--width", `65%`);
+//   fifty.style.backgroundColor = "#3366FF";
+// });
 
-thirdPrev.addEventListener("click", (e) => {
-  e.preventDefault();
-  thirdStep.classList.add("none");
-  secondStep.classList.remove("none");
-  successLine.style.setProperty("--width", `35%`);
-  fifty.style.backgroundColor = "transparent";
-});
+// thirdPrev.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   thirdStep.classList.add("none");
+//   secondStep.classList.remove("none");
+//   successLine.style.setProperty("--width", `35%`);
+//   fifty.style.backgroundColor = "transparent";
+// });
 
-thirdNext.addEventListener("click", (e) => {
-  e.preventDefault();
-  thirdStep.classList.add("none");
-  fourthStep.classList.remove("none");
-  successLine.style.setProperty("--width", `100%`);
-  seventyFive.style.backgroundColor = "#3366FF";
-});
+// thirdNext.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   thirdStep.classList.add("none");
+//   fourthStep.classList.remove("none");
+//   successLine.style.setProperty("--width", `100%`);
+//   seventyFive.style.backgroundColor = "#3366FF";
+// });
