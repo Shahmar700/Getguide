6407bdfcb8b5e8061cdea18d59612d8a7686d880
Shahmar <shahmar700@gmail.com>
2023-03-23 12:01:30 +0400

    upgrade guides filter

--- JS/guidesFilter.js
+++ JS/guidesFilter.js
@@ -1,32 +1,9 @@
 // Giudes Filters
-
-const langFilterHead = document.querySelectorAll(".langFilterHead");
-const langChevron = document.querySelector("#langChevron");
-[...langFilterHead].map((h) => {
-  h.addEventListener("click", () => {
-    let dropdown = h.nextElementSibling;
-    dropdown.classList.toggle("dropdownHeight");
-    langChevron.classList.toggle("rotate180");
-  });
-});
-
-const activityFilterHead = document.querySelectorAll(".activityFilterHead");
-const activityChevron = document.querySelector("#activityChevron");
-[...activityFilterHead].map((h) => {
-  h.addEventListener("click", () => {
-    let dropdown = h.nextElementSibling;
-    dropdown.classList.toggle("dropdownHeight");
-    activityChevron.classList.toggle("rotate180");
-  });
-});
-
-const priceFilterHead = document.querySelectorAll(".priceFilterHead");
-const priceChevron = document.querySelector("#priceChevron");
-[...priceFilterHead].map((h) => {
-  h.addEventListener("click", () => {
-    let dropdown = h.nextElementSibling;
-    dropdown.classList.toggle("dropdownHeight");
-    priceChevron.classList.toggle("rotate180");
+const filterHeader = document.querySelectorAll(".filterHeader");
+filterHeader.forEach((e) => {
+  e.addEventListener("click", () => {
+    e.nextElementSibling.classList.toggle("dropdownHeight");
+    e.lastElementChild.classList.toggle("rotate180");
   });
 });
 
